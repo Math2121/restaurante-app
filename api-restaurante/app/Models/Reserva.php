9da5db851d84eae73fd957e957fc75defa31ab65
Matheus de Paula <matheusdepaula527@gmail.com>
2023-05-13 17:08:10 -0300

    criando migration e model

---
+++ api-restaurante/app/Models/Reserva.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reserva extends Model
+{
+    use HasFactory;
+
+    const NAME = 'nome';
+    const HORARIO = 'horario';
+    const DIA = 'dia';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        self::NAME,
+        self::HORARIO,
+        self::DIA,
+    ];
+
+
+}
