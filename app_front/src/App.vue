700feaef73c6735cfbc4a309a07c3f52d2ba6929
Matheus de Paula <matheusdepaula527@gmail.com>
2023-05-14 20:47:06 -0300

    iniciando front da aplicação

---
+++ app_front/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="justify-center flex bg-blue-300 items-center h-screen">
+    <div class="text-4xl">
+      Hello Welcome to Tailwind Css
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
