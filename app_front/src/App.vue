89d649dd533d5cb7c4ca0c5e208302bdf926b762
Matheus de Paula <matheusdepaula527@gmail.com>
2023-05-15 15:09:31 -0300

    configurando aplicação

--- app_front/src/App.vue
+++ app_front/src/App.vue
@@ -1,4 +1,39 @@
 <template>
-    <router-view></router-view>
-</template>
+  <nav class="bg-white border-gray-200 dark:bg-emerald-500">
+    <div
+      class=" flex flex-wrap items-center justify-between mx-auto p-4 container"
+    >
 
+      <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+        <ul
+          class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-emerald-500 md:dark:bg-emerald-500 dark:border-gray-700"
+        >
+          <li>
+            <a
+              href="/login"
+              class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+              aria-current="page"
+              >Login</a
+            >
+          </li>
+          <li>
+            <a
+              href="/register"
+              class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >Cadastro</a
+            >
+          </li>
+          <li>
+            <a
+              href="/cadastro-reservas"
+              class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >Fazer Reservas</a
+            >
+          </li>
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <router-view></router-view>
+</template>
--- app_front/src/router/index.js
+++ app_front/src/router/index.js
@@ -1,18 +1,58 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Register from '../views/Register.vue'
-
+import Login from '../views/Login.vue'
+import Reservas from '../views/Reservas.vue'
+import CadastroReservas from '../views/CadastroReservas.vue'
+import Unauthorized from '../views/Unauthorized.vue'
+import { getToken } from '../utils/utils'
 const routes = [
   {
-    path: '/',
+    path: '/register',
     name: 'Register',
     component: Register
   },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/',
+    name: 'CadastroReservas',
+    component: CadastroReservas
 
+  },
+  {
+    path: '/unauthorized',
+    name: 'Unauthorized',
+    component: Unauthorized
+
+  },
+  {
+    path: '/reservas',
+    name: 'Reservas',
+    component: Reservas,
+    meta: {
+      resource: 'ACL'
+    }
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+
+router.beforeEach((to, _, next) => {
+
+  const token = getToken();
+  console.log(token)
+  if (to.meta.resource === 'ACL') {
+    if (!token) return next({ name: 'Unauthorized' })
+  }
+
+  return next()
+});
 
 export default router
