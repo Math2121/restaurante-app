74c827c672aa0f478d7f407bbd5791a912b4b991
Matheus de Paula <matheusdepaula527@gmail.com>
2023-05-15 00:10:59 -0300

    criando páginas restantes

---
+++ app_front/src/views/Login.vue
@@ -0,0 +1,58 @@
+<template>
+	<div
+	  class="relative flex flex-col justify-center min-h-screen overflow-hidden"
+	>
+	  <div
+		class="w-full p-6 m-auto bg-white rounded shadow-lg ring-2 ring-purple-800/50 lg:max-w-md"
+	  >
+		<h1 class="text-3xl font-semibold text-center text-purple-700">
+		  Login
+		</h1>
+  
+		<form class="mt-6" @submit.prevent="submitRegister">
+		  <div>
+		  <Input label="Email"  type="email" v-model="email"/>
+		  </div>
+		  <div class="mt-4">
+			<div>
+			  <Input label="Senha" type="password" v-model="senha"/>
+			</div>
+		  </div>
+			<div class="mt-6">
+			  <button
+				class="w-full px-4 py-2 text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 "
+			  >
+				Entrar
+			  </button>
+			</div>
+	  
+		</form>
+	  </div>
+	</div>
+  </template>
+  
+  <script>
+  import Input from "../components/Input.vue";
+  export default {
+	components: {
+	  Input,
+	},
+	name: "Login",
+	data(){
+	  return {
+		  email: '',
+		  senha: ''
+	  }
+	},
+	methods: {
+	  submitRegister() {
+		  console.log('je')
+  
+  
+		  console.log(this.email)
+		  console.log(this.senha)
+	  }
+	}
+  };
+  </script>
+  
---
+++ app_front/src/views/Reservas.vue
@@ -0,0 +1,44 @@
+
+<template>	
+<div class="flex flex-col justify-center min-h-screen overflow-hidden">
+  <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+    <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+      <div class="overflow-hidden">
+        <table class="min-w-full text-left text-sm font-light">
+          <thead class="border-b font-medium dark:border-neutral-500">
+            <tr>
+              <th scope="col" class="px-6 py-4">Nome</th>
+              <th scope="col" class="px-6 py-4">Dia</th>
+              <th scope="col" class="px-6 py-4">Horário</th>
+
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="border-b dark:border-neutral-500">
+              <td class="whitespace-nowrap px-6 py-4 font-medium">1</td>
+              <td class="whitespace-nowrap px-6 py-4">Mark</td>
+              <td class="whitespace-nowrap px-6 py-4">Otto</td>
+
+            </tr>
+            <tr class="border-b dark:border-neutral-500">
+              <td class="whitespace-nowrap px-6 py-4 font-medium">2</td>
+              <td class="whitespace-nowrap px-6 py-4">Jacob</td>
+              <td class="whitespace-nowrap px-6 py-4">Thornton</td>
+
+            </tr>
+  
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+
+export default {
+	name: 'Reservas',
+	props: {}
+}
+</script>
