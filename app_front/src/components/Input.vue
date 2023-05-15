652e82899bee9ec1464eb6540aded9e49d706503
Matheus de Paula <matheusdepaula527@gmail.com>
2023-05-15 15:10:41 -0300

    configuração de aplicação e pagina de reservas

--- app_front/src/components/Input.vue
+++ app_front/src/components/Input.vue
@@ -6,7 +6,7 @@
       :type="type"
 
       @input="$emit('update:modelValue', $event.target.value)"
-      class="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+      class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-emerald-400 focus:ring-emerald-300 focus:outline-none focus:ring focus:ring-opacity-40"
 	  required
     />
   </div>
--- app_front/src/views/CadastroReservas.vue
+++ app_front/src/views/CadastroReservas.vue
@@ -3,38 +3,34 @@
 	  class="relative flex flex-col justify-center min-h-screen overflow-hidden"
 	>
 	  <div
-		class="w-full p-6 m-auto bg-white rounded shadow-lg ring-2 ring-purple-800/50 lg:max-w-md"
+		class="w-full p-6 m-auto bg-white rounded shadow-lg ring-2 emerald-500/50 lg:max-w-md"
 	  >
-		<h1 class="text-3xl font-semibold text-center text-purple-700">
-		  Cadastro
+		<h1 class="text-3xl font-semibold text-center text-emerald-700">
+		  Fazer Reserva
 		</h1>
   
 		<form class="mt-6" @submit.prevent="submitRegister">
 		  <div>
-		  <Input label="Email"  type="email" v-model="email"/>
+			<Input label="Email" type="email" v-model="email" />
 		  </div>
 		  <div class="mt-4">
 			<div>
-			  <Input label="Senha" type="password" v-model="senha"/>
+			  <Input label="Senha" type="password" v-model="senha" />
 			</div>
 		  </div>
-			<div class="mt-6">
-			  <button
-				class="w-full px-4 py-2 text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 "
-			  >
-				Cadastrar
-			  </button>
-			</div>
-	  
+		  <div class="mt-6">
+			<button
+			  class="w-full px-4 py-2 text-white transition-colors duration-200 transform bg-emerald-500 rounded-md hover:bg-emerald-200"
+			  :disabled="isDisabled"
+			>
+			  Reservar
+			</button>
+		  </div>
 		</form>
-		<p class="mt-8 text-xs font-light text-center text-gray-700">
-		  <a href="#" class="font-medium text-purple-600 hover:underline"
-			>Login</a
-		  >
-		</p>
 	  </div>
 	</div>
   </template>
+  
   
   <script>
   import Input from "../components/Input.vue";
